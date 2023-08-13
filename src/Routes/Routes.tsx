998009a5c02006a5d80509165c55074da40c8705
unknown <tobiburrohman2@gmail.com>
2023-08-13 08:18:56 +0600

    register page completely designed

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,36 @@
+
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
+    },
+    {
+        path:'/register',
+        element:<Register/>
+    },
+    {
+        path:'/login',
+        element:<Login/>
+    },
+    {
+        path:'*',
+        element:<NotFound/>
+    },
+]);
+
+export default routes
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { DatePicker } from "antd"
+import Navbar from "../components/Layout/Navbar"
+import Banner from "../components/banner"
+import RootLayout from "../components/Layout/RootLayout"
+
+
+const Home = () => {
+  return (
+    <>
+      <RootLayout>
+        <h1>Hello</h1>
+      </RootLayout>
+    </>
+  )
+}
+
+export default Home
